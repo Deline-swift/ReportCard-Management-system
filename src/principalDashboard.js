cf0498c8ca43332a4969bf698dd798e5a1130af9
Deline-Swift <delineswift54@gmail.com>
2025-04-23 10:15:17 -0400

    first commit

---
+++ src/principalDashboard.js
@@ -0,0 +1,7 @@
+const student_name = document.getElementById("studentName").value;
+const place_birth = document.getElementById("place_of_birth").value;
+const createBtn = document.getElementById("create")
+
+createBtn.addEventListener("click",function(){
+    alert("clicked")
+})
